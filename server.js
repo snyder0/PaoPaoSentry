8e8e6587c4edae73c84dd8cd12a444e6765834be
Snyder0 <luke.snyder@selu.edu>
2020-02-04 18:23:07 -0600

    add object detection

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const path =  require('path')
 const cv = require('opencv4nodejs')
 const express = require('express')
+const { drawRectAroundBlobs } = require('./utils');
 
 console.log("starting server...")
 
@@ -22,6 +23,20 @@ wCap.set(cv.CAP_PROP_FRAME_HEIGHT, 300)
 
 setInterval(() => {
     const frame = wCap.read()
+
+    const frameHLS = frame.cvtColor(cv.COLOR_BGR2HLS);
+
+    const brownUpper = new cv.Vec(10, 60, 165);
+    const brownLower = new cv.Vec(5, 20, 100);
+    const rangeMask = frameHLS.inRange(brownLower, brownUpper);
+
+    const blurred = rangeMask.blur(new cv.Size(10, 10));
+    const thresholded = blurred.threshold(100, 255, cv.THRESH_BINARY);
+
+    const minPxSize = 400;
+    const fixedRectWidth = 100;
+    drawRectAroundBlobs(thresholded, frame, minPxSize, fixedRectWidth);
+
     const image = cv.imencode('.jpg', frame).toString('base64')
     io.emit('image', image)
 }, 1000 / FPS)
